2740353c8cbd836ce16469975e6b8ff7cd529d98
maruf <mdmarufbinsalim@gmail.com>
2026-04-12 04:12:53 +0600

    feat: formated source for route transition

--- src/providers/RouteTransitionProvider.tsx
+++ src/providers/RouteTransitionProvider.tsx
@@ -4,72 +4,72 @@ import { AnimatePresence, Easing, motion } from "framer-motion";
 import { usePathname } from "next/navigation";
 import { useEffect, useRef, useState } from "react";
 
+export default function RouteTransitionProvider({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+
+    const intialRender = useRef(true);
+
+    const pathname = usePathname();
+    const [isAnimating, setIsAnimating] = useState(false);
+    const [childrenNode, setChildrenNode] = useState<React.ReactNode>(null);
+    useState<React.ReactNode>(children);
+
+    useEffect(() => {
+        if (intialRender.current) {
+            setChildrenNode(children);
+            intialRender.current = false;
+            return;
+        }
+
+        setIsAnimating(true);
+        setChildrenNode(null);
+
+        const timeout = setTimeout(() => {
+            setChildrenNode(children);
+            setIsAnimating(false);
+        }, CONFIG.ANIMATION_DURATION.MS);
+
+        return () => clearTimeout(timeout);
+    }, [pathname, children]);
+
+    return (
+        <>
+            {childrenNode}
+
+            <AnimatePresence>
+                {isAnimating && (
+                    <motion.div
+                        key="route-transition"
+                        initial={{ y: "100%" }}
+                        animate={{ y: 0 }}
+                        exit={{ y: "-100%" }}
+                        transition={{
+                            duration: CONFIG.ANIMATION_DURATION.S,
+                            ease: CONFIG.ANIMATION_EASING,
+                        }}
+                        className="fixed top-0 left-0 z-[9999] h-screen w-screen bg-white"
+                    />
+                )}
+            </AnimatePresence>
+        </>
+    );
+}
+
 type ConfigType = {
-  ANIMATION_DURATION: {
-    MS: number;
-    S: number;
-  };
-  ANIMATION_EASING: Easing | Easing[];
+    ANIMATION_DURATION: {
+        MS: number;
+        S: number;
+    };
+    ANIMATION_EASING: Easing | Easing[];
 };
 
 const CONFIG: ConfigType = {
-  ANIMATION_DURATION: {
-    MS: 600,
-    S: 0.6,
-  },
-  ANIMATION_EASING: [0.76, 0, 0.24, 1],
+    ANIMATION_DURATION: {
+        MS: 600,
+        S: 0.6,
+    },
+    ANIMATION_EASING: [0.76, 0, 0.24, 1],
 };
-
-export default function RouteTransitionProvider({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  const pathname = usePathname();
-
-  const intialRender = useRef(true);
-
-  const [isAnimating, setIsAnimating] = useState(false);
-  const [childrenNode, setChildrenNode] = useState<React.ReactNode>(children);
-    useState<React.ReactNode>(children);
-
-  useEffect(() => {
-
-    if (intialRender.current) {
-      intialRender.current = false;
-      return;
-    }
-
-    setIsAnimating(true);
-    setChildrenNode(null);
-
-    const timeout = setTimeout(() => {
-      setChildrenNode(children);
-      setIsAnimating(false);
-    }, CONFIG.ANIMATION_DURATION.MS);
-
-    return () => clearTimeout(timeout);
-  }, [pathname, children]);
-
-  return (
-    <>
-      {childrenNode}
-
-      <AnimatePresence>
-        {isAnimating && (
-          <motion.div
-            key="route-transition"
-            initial={{ y: "100%" }}
-            animate={{ y: 0 }}
-            exit={{ y: "-100%" }}
-            transition={{
-              duration: CONFIG.ANIMATION_DURATION.S,
-              ease: CONFIG.ANIMATION_EASING,
-            }}
-            className="fixed top-0 left-0 z-[9999] h-screen w-screen bg-white"
-          />
-        )}
-      </AnimatePresence>
-    </>
-  );
-}
